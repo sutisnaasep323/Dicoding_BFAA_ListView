784722edfbf05d8f870251b23f95ecd18e151a5d
Asep Sutisna <sutisnaasep323@gmail.com>
2021-08-14 14:41:55 +0700

    Dicoding_BFAA_ListView_Finish

--- app/src/main/java/com/example/mylistview/MainActivity.kt
+++ app/src/main/java/com/example/mylistview/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.mylistview
 
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private val dataName = arrayOf("Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Pattimura", "R A Kartini", "Sukarno")
+    // testing - > private val dataName = arrayOf("Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Pattimura", "R A Kartini", "Sukarno")
+    private lateinit var adapter : HeroAdapter
+    private lateinit var dataName: Array<String>
+    private lateinit var dataDescription: Array<String>
+    private lateinit var dataPhoto: TypedArray
+    private var heroes = arrayListOf<Hero>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView : ListView = findViewById(R.id.lv_list)
-        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, dataName)
+        //testing
+//        val listView : ListView = findViewById(R.id.lv_list)
+//        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, dataName)
+//        listView.adapter = adapter
+
+        val listView: ListView = findViewById(R.id.lv_list)
+        adapter = HeroAdapter(this)
         listView.adapter = adapter
+
+        prepare()   // siapkan data
+        addItem() // masukkan data tsb dg looping
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            Toast.makeText(this@MainActivity, heroes[position].name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun addItem() {
+        for (position in dataName.indices) {
+            val hero = Hero(
+                dataPhoto.getResourceId(position, -1),
+                dataName[position],
+                dataDescription[position]
+            )
+            heroes.add(hero)
+        }
+        adapter.heroes = heroes
+    }
+
+    private fun prepare() {
+        dataName = resources.getStringArray(R.array.data_name)
+        dataDescription = resources.getStringArray(R.array.data_description)
+        dataPhoto = resources.obtainTypedArray(R.array.data_photo)
     }
 }
