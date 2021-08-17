5b20a1f51193622950a13190dfd733a2830fe0ca
Asep Sutisna <sutisnaasep323@gmail.com>
2021-08-17 09:40:43 +0700

    Dicoding_BFAA_MyListView_Refactor_ViewBinding

--- app/src/main/java/com/example/mylistview/HeroAdapter.kt
+++ app/src/main/java/com/example/mylistview/HeroAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.mylistview.databinding.ItemHeroBinding
 import de.hdodenhof.circleimageview.CircleImageView
 
 class HeroAdapter internal constructor(private val context: Context): BaseAdapter(){
@@ -49,13 +50,20 @@ class HeroAdapter internal constructor(private val context: Context): BaseAdapte
     }
 
     private inner class ViewHolder internal constructor(view: View) {
-        private val txtName: TextView = view.findViewById(R.id.txt_name)
-        private val txtDescription: TextView = view.findViewById(R.id.txt_description)
-        private val imgPhoto: CircleImageView = view.findViewById(R.id.img_photo)
+        //kode ini akan di refactor menggunakan view binding
+//        private val txtName: TextView = view.findViewById(R.id.txt_name)
+//        private val txtDescription: TextView = view.findViewById(R.id.txt_description)
+//        private val imgPhoto: CircleImageView = view.findViewById(R.id.img_photo)
+        
+        private val binding = ItemHeroBinding.bind(view)
+        
         internal fun bind(hero: Hero) {
-            txtName.text = hero.name
-            txtDescription.text = hero.description
-            imgPhoto.setImageResource(hero.photo)
+//            txtName.text = hero.name
+//            txtDescription.text = hero.description
+//            imgPhoto.setImageResource(hero.photo)
+            binding.txtName.text = hero.name
+            binding.txtDescription.text = hero.description
+            binding.imgPhoto.setImageResource(hero.photo)
         }
     }
 }
